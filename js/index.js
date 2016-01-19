1c5087cd0e983ca12c8dfa7de09bb9ffb7818195
Min Jung Park <mp3369@columbia.edu>
2016-01-18 20:08:24 -0500

    fixed typo

--- js/index.js
+++ js/index.js
@@ -1,12 +1,10 @@
-$(document).ready(function()
-{
+$(document).ready(function(){
 
-	$('.tabs .tab-links a').on('click', function(e)
+	$('.tab-links a').on('click', function()
 	{
 		var current = $(this).attr('href');
-		$('.tabs' + current).fadeIn(400).siblings().hide();
+		$('.tab' + current).fadeIn(400).siblings().hide();
 		$(this).parent('li').addClass('active').siblings().removeClass('active');
-		e.preventDefaut();
 
 	});
 });
