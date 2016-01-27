f25e62d98d4a708a4b968142f9be4be4f65af921
Min Jung Park <mp3369@columbia.edu>
2016-01-27 01:02:09 -0500

    fixed content

--- js/index.js
+++ js/index.js
@@ -3,9 +3,8 @@ $(document).ready(function(){
 	$('.tab-links a').on('click', function()
 	{
 		var current = $(this).attr('href');
-		$('.tab' + current).fadeIn(400).siblings().hide();
+		$('.tab' + current).slideDown(400).siblings().hide();
 		$(this).parent('li').addClass('active').siblings().removeClass('active');
 		$('.intro').hide();
-
 	});
 });
